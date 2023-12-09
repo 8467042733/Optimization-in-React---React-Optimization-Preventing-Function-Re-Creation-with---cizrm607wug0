8a4e830057280855b056a3e7b10477d9ce94dc59
8467042733 <rupeshpal25112004@gmail.com>
2023-12-09 17:01:52 +0530

    some chnges in code to the run

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ const isPrimeNumber = (n) => {
 };
 const isArmstrongNumber = (number) => {
   console.log("Expensive calculation execution for Armstrong");
-  const numberOfDigits = $number.length;
+  const numberOfDigits = `${number}`.length;
   let sum = 0;
   let temp = number;
   while (temp > 0) {
@@ -38,16 +38,33 @@ const Home = () => {
     <div className="App">
       <h2>Armstrong Checker</h2>
       <br />
-      <input id="input-1"value={num1}onChange={(e) => setNum1(+e.target.value)}
+      <input
+        id="input-1"
+        value={num1}
+        onChange={(e) => setNum1(+e.target.value)}
       />
-      <h2 id="answer-1"> num1 ? isArmstrong ? ${num1} is armstrong : ${num1} is not armstrong : `Please Enter Number `;
+      <h2 id="answer-1">
+        {num1
+          ? isArmstrong
+            ? `${num1} is armstrong`
+            : `${num1} is not armstrong`
+          : `Please Enter Number `}
       </h2>
       ----------------------------------------------
       <br />
       <h2>Prime Checker</h2>
       <br />
-      <input id="input-2" value={num2}onChange={(e) => setNum2(+e.target.value)}/>
-      <h2 id="answer-2">num2? isPrime? ${num2} is Prime : ${num2} is not Prime : `Please Enter Number `;
+      <input
+        id="input-2"
+        value={num2}
+        onChange={(e) => setNum2(+e.target.value)}
+      />
+      <h2 id="answer-2">
+        {num2
+          ? isPrime
+            ? `${num2} is Prime`
+            : `${num2} is not Prime`
+          : `Please Enter Number `}
       </h2>
     </div>
   );
